f26e59d490f08afb70e2e92f8c6af2932ee640ab
Nithin Krishna <nkrishna6238@gmail.com>
2025-03-23 19:50:00 -0400

    Closes #10, Implemented Bookmarking functionality.

--- loadBookmarkedArticles.js
+++ loadBookmarkedArticles.js
@@ -4,9 +4,6 @@ import { auth } from "./firebase-config.js";
 const db = getFirestore();
 const bookmarkedArticlesList = document.getElementById("bookmarked-articles-list");
 
-/**
- * Function to load bookmarked articles with tags.
- */
 auth.onAuthStateChanged(async (user) => {
   if (user) {
     loadBookmarkedArticles(user.uid);
